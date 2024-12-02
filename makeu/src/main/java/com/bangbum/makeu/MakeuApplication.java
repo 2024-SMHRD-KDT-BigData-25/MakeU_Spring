dd3be2745c8addf98b0d6a044b82a93ed4f0734f
eycho <chw3872@gmail.com>
2024-12-02 19:36:35 +0900

    은유 spring project 설정

---
+++ makeu/src/main/java/com/bangbum/makeu/MakeuApplication.java
@@ -0,0 +1,13 @@
+package com.bangbum.makeu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakeuApplication.class, args);
+	}
+
+}
---
+++ makeu/src/test/java/com/bangbum/makeu/MakeuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bangbum.makeu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakeuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
